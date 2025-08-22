da6f7b112e62a30a09c376c4f0762163bb6ef6f5
Colin Whiteford <colinwhiteford516@gmail.com>
2025-08-21 21:39:50 -0400

    Added Healing and Invulnerability to Health

--- Assets/Script/Health/Health.cs
+++ Assets/Script/Health/Health.cs
@@ -7,7 +7,11 @@ public class Health : MonoBehaviour, IDamageable
     [field:SerializeField] public uint CurrentHealth { get; private set; }
     [field:SerializeField] public uint MaxHealth { get; private set; }
     public UnityEvent OnDamagedEvent = new();
-    
+    public UnityEvent OnHealedEvent = new();
+    public UnityEvent OnDeathEvent = new();
+    public UnityEvent OnReviveEvent = new();
+    public bool IsAlive => CurrentHealth > 0;
+    public bool IsInvulnerable { get; private set; }
     public void Awake()
     {
         CurrentHealth = MaxHealth;
@@ -15,7 +19,34 @@ public class Health : MonoBehaviour, IDamageable
     
     public void Damage(uint damage)
     {
+        if (IsInvulnerable)
+            return;
+        bool currentAliveState = IsAlive;
         CurrentHealth -= CurrentHealth > damage ? damage : CurrentHealth;
         OnDamagedEvent.Invoke();
+        if (currentAliveState != IsAlive)
+        {
+            OnDeathEvent.Invoke();
+        }
     }
+    public void Heal(uint heal)
+    {
+        bool currentAliveState = IsAlive;
+        CurrentHealth += CurrentHealth < MaxHealth ? heal : MaxHealth;
+        OnHealedEvent.Invoke();
+        if (currentAliveState != IsAlive)
+        {
+            OnReviveEvent.Invoke();
+        }
+    }
+    void MakeInvulnerable()
+    {
+        IsInvulnerable = true;
+    }
+
+    void MakeVulnerable()
+    {
+        IsInvulnerable = false;
+    }
+    
 }

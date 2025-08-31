a2be9fc689e665348cbbeedebd63b0d45d06a1e6
Colin Whiteford <colinwhiteford516@gmail.com>
2025-08-31 19:43:30 -0400

    Health Pickup Has Bobbing, Spinning, and Model

--- Assets/Script/Items/HealthPickup.cs
+++ Assets/Script/Items/HealthPickup.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class HealthPickup : MonoBehaviour
 {
+    [SerializeField] float m_rotationSpeed = 100;
+    [SerializeField] float m_bobbingSpeed = 1;
+    [SerializeField] float m_bobbingAmount = 0.25f;
     [SerializeField] uint m_healAmount = 10;
     void OnTriggerEnter(Collider other)
     {
@@ -14,6 +17,7 @@ public class HealthPickup : MonoBehaviour
     }
     void Update()
     {
-        transform.Rotate(Vector3.up, 100 * Time.deltaTime);
+        transform.Rotate(Vector3.up, m_rotationSpeed * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, transform.position.y + (Mathf.Sin(Time.time * m_bobbingSpeed) * Time.deltaTime * m_bobbingAmount), transform.position.z);
     }
 }

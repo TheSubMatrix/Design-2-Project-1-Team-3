1800f0c6f526de9846b451cbce963615e0fa61ff
Colin Whiteford <colinwhiteford516@gmail.com>
2025-08-31 18:36:32 -0400

    Spell Pickup Now Moves and Has Correct Mesh

--- Assets/Script/Items/NewWeaponItem.cs
+++ Assets/Script/Items/NewWeaponItem.cs
@@ -7,13 +7,12 @@ public class NewWeaponItem : MonoBehaviour
 {
     [SerializeField] float m_rotationSpeed;
     [SerializeField]StaffSpellSO m_staffSpellToGive;
-
+    float m_yPos;
     void OnTriggerEnter(Collider other)
     {
         Staff staff = other.gameObject.GetComponentInChildren<CameraControl>()?.m_cam.GetComponentInChildren<Staff>();
         if (staff is null)
         {
-            Debug.Log("No staff found");
             return;
         }
         Staff.SpellSlot[] matchingSlots = staff.SpellSlots
@@ -37,8 +36,14 @@ public class NewWeaponItem : MonoBehaviour
         }
     }
 
+    void Start()
+    {
+        m_yPos = transform.position.y;
+    }
+
     void Update()
     {
         transform.Rotate(Vector3.up, 100 * Time.deltaTime);
+        transform.position = new Vector3(transform.position.x, m_yPos + (Mathf.Sin(Time.time) * 0.25f), transform.position.z);
     }
 }

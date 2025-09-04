df2369814cf44a525a43edd0c231b53399286d55
GreenIsaiah <ikragland@my.waketech.edu>
2025-09-03 20:59:31 -0400

    added level scenes
    added level scenes

--- Assets/Script/obstacles/FireBar.cs
+++ Assets/Script/obstacles/FireBar.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class FireBar : MonoBehaviour
 {
-    private float fanSpeed = 100f;
+    private float fanSpeed = 75f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {

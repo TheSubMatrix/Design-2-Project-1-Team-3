f5ee2340cd7ff718ec650b86bd0a99eb4cf63b32
GreenIsaiah <ikragland@my.waketech.edu>
2025-09-05 19:47:22 -0400

    axes now damage player
    axes now damage player

--- Assets/Script/obstacles/rotateAxes.cs
+++ Assets/Script/obstacles/rotateAxes.cs
@@ -2,20 +2,10 @@ using UnityEngine;
 
 public class rotateAxes : MonoBehaviour
 {
-    private float rotationSpeed = 100f; //Speed
-    private float targetAngle = 60f; //rotation
-
-    void Update()
-    {
-        float angle = Mathf.PingPong(Time.time * rotationSpeed, targetAngle);
-        transform.rotation = Quaternion.Euler(0, 0, angle); // Rotates on the Y-axis
-    }
+    [SerializeField] uint m_attackDamage;
     public void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            //player takes damage
-            Debug.Log("player took damage");
-        }
+        other.gameObject.GetComponent<IDamageable>()?.Damage(m_attackDamage);
+        Debug.Log(other.gameObject.GetComponent<IDamageable>());
     }
 }

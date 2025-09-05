632dc5d0222b17cf1264bc0e042c6fcfed074ad8
Colin Whiteford <colinwhiteford516@gmail.com>
2025-09-05 15:36:42 -0400

    Health Pickup VFX and Health Bar Level 1 Fix

--- Assets/Script/UI/HealthBar.cs
+++ Assets/Script/UI/HealthBar.cs
@@ -6,16 +6,17 @@ public class HealthBar : MonoBehaviour
 {
     uint m_currentHealth;
     uint m_maxHealth;
-    [SerializeField] private Image m_healthBar;
+    [SerializeField] Image m_healthBar;
     public void InitializeHealthBar(uint currentHealth, uint maxHealth)
     {
         m_currentHealth = currentHealth;
         m_maxHealth = maxHealth;
-        m_healthBar.fillAmount = (float)m_currentHealth / (float)m_maxHealth;
+        
+        m_healthBar.fillAmount = m_currentHealth / (float)m_maxHealth;
     }
     public void UpdateHealthBar(uint oldHealth, uint newHealth)
     {
         m_currentHealth = newHealth;
-        m_healthBar.fillAmount = (float)m_currentHealth / (float)m_maxHealth;
+        m_healthBar.fillAmount = newHealth / (float)m_maxHealth;
     }
 }

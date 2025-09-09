dffaec5b358135579574ba6236d0e4b735e9845c
Colin Whiteford <colinwhiteford516@gmail.com>
2025-09-09 16:45:53 -0400

    Cleaned up Key Script

--- Assets/Script/obstacles/KeyPickUp.cs
+++ Assets/Script/obstacles/KeyPickUp.cs
@@ -3,23 +3,10 @@ using UnityEngine;
 public class DoorAndKey : MonoBehaviour
 {
     public int key = 0;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            Destroy(gameObject);
-            key += 1;
-        }
+        if (other.gameObject.layer != LayerMask.NameToLayer("Player")) return;
+        Destroy(gameObject);
+        key += 1;
     }
 }

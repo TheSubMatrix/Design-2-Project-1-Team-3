7c8eaa23c0b8a524fbf4503879d8cce70e6ee269
GreenIsaiah <ikragland@my.waketech.edu>
2025-09-10 19:42:42 -0400

    Door Script changed
    Door Script changed at line 10

--- Assets/Script/obstacles/Door.cs
+++ Assets/Script/obstacles/Door.cs
@@ -7,7 +7,7 @@ public class Door : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        keyPickUp.key += 1;
+       //keyPickUp.key += 1;
     }
 
     // Update is called once per frame

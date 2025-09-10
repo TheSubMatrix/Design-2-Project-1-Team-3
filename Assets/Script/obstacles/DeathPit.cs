6cae35f53616a0f54e4235f1ad9038833aab765c
GreenIsaiah <ikragland@my.waketech.edu>
2025-09-10 17:46:45 -0400

    Made animations for platformsLvl3
    Made animations for platformsLvl3

--- Assets/Script/obstacles/DeathPit.cs
+++ Assets/Script/obstacles/DeathPit.cs
@@ -6,7 +6,6 @@ public class DeathPit : MonoBehaviour
     public void OnTriggerEnter(Collider other)
     {
         other.gameObject.GetComponent<IDamageable>()?.Damage(m_attackDamage);
-        Debug.Log(other.gameObject.GetComponent<IDamageable>());
 
     }
 }

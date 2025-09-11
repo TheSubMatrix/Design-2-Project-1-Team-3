03aac69cbd7e507051cf6c3a9a883a6d2f47bb8e
GreenIsaiah <ikragland@my.waketech.edu>
2025-09-10 22:02:54 -0400

    add curse event on credits preplace my pillars, made fan do damage
    add mouse.curse event on credits preplace my pillars, made fan do damage.

--- Assets/Script/obstacles/Teleport.cs
+++ Assets/Script/obstacles/Teleport.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
+    private Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     public void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.layer != LayerMask.NameToLayer("Player"))
+        if(other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
-            transform.position = new Vector3(475.7f, 105f, -71.2f);
+            other.gameObject.transform.position  = new Vector3(-475.7f, 105f, -71.2f);
         }
     }
 }

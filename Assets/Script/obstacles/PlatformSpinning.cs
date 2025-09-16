8e832896bf2827774a7d34547c60c7fc6de9308e
Colin Whiteford <colinwhiteford516@gmail.com>
2025-09-15 20:27:20 -0400

    Fixed Isiah's Platform Rotation

--- Assets/Script/obstacles/PlatformSpinning.cs
+++ Assets/Script/obstacles/PlatformSpinning.cs
@@ -2,16 +2,26 @@ using UnityEngine;
 
 public class PlatformSpinning : MonoBehaviour
 {
-    private float fanSpeed = 30f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] float m_fanSpeed = 30f;
+    [SerializeField] Rigidbody m_rigidbody;
+    [SerializeField] float m_angle = 0f;
+    Vector3 m_initialOffset;
+
     void Start()
     {
-
+        if (!m_rigidbody) m_rigidbody = GetComponent<Rigidbody>();
+        m_initialOffset = m_rigidbody.position - transform.position;
+        if (m_initialOffset == Vector3.zero) m_initialOffset = Vector3.right; // ensure non-zero radius
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.Rotate(0, fanSpeed * Time.deltaTime, 0);
+        m_angle += m_fanSpeed * Time.fixedDeltaTime;
+        if (m_angle >= 360f) m_angle -= 360f;
+
+        Quaternion rot = Quaternion.AngleAxis(m_angle, Vector3.up);
+        Vector3 targetPos = transform.position + rot * m_initialOffset;
+        m_rigidbody.MovePosition(targetPos);
+        m_rigidbody.MoveRotation(rot);
     }
 }

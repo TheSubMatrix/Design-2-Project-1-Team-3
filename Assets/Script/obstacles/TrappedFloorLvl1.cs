57b347d16d870752b9df075fedb84a6436e12992
GreenIsaiah <ikragland@my.waketech.edu>
2025-09-15 20:08:22 -0400

    Level 1 trapped door open script
    Level 1 trapped door script

--- Assets/Script/obstacles/TrappedFloorLvl1.cs
+++ Assets/Script/obstacles/TrappedFloorLvl1.cs
@@ -1,13 +1,33 @@
 using UnityEngine;
+using System.Collections;
 
 public class TrappedFloorLvl1 : MonoBehaviour
 {
+    public float rotationSpeed = 1.0f;
+    private bool isRotating = false;
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
+            StartCoroutine(RotateObject(Vector3.up, 90));
             //Destroy(gameObject);
-            transform.Rotate(0, 0, 90);
+            transform.Rotate(90, 0, 0);
         }
+    }
+    private IEnumerator RotateObject(Vector3 axis, float angle)
+    {
+        isRotating = true;
+        Quaternion startRotation = transform.rotation;
+        Quaternion endRotation = Quaternion.Euler(transform.eulerAngles + axis * angle);
+        float t = 0;
+
+        while (t < 1.0f)
+        {
+            t += Time.deltaTime * rotationSpeed;
+            transform.rotation = Quaternion.Lerp(startRotation, endRotation, t);
+            yield return null;
+        }
+
+        isRotating = false;
     }
 }
